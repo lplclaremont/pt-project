e2288795a7f4b664fcc02d450215dfc5d3c7a7ab
Louis Claremont <lpclaremont@gmail.com>
2023-11-02 12:27:57 +0000

    image and summary now appear on homepage next to each other

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -4,14 +4,21 @@ import React from 'react';
 import lauraHomepage from '../../assets/lauraHomepage.jpeg';
 
 function Home() {
-  return (
-    <div className="Home">
-        <h1>Start your journey towards healthier, happier living</h1>
-        <div className="image-container">
-            <img src={lauraHomepage} alt="Laura" />
+    return (
+        <div className="Home">
+            <h1>Start your journey towards healthier, happier living</h1>
+            <div className="summaryContainer">
+                <div className="textSummary">
+                    {/* Add your text summary here */}
+                    <p>Your text summary goes here.</p>
+                </div>
+                <div className="image-container">
+                    {/* Add your image here */}
+                    <img src={lauraHomepage} alt="Laura" />
+                </div>
+            </div>
         </div>
-    </div>
-  );
-}
+    );
+  }
 
 export default Home;
