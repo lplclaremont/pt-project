a5515f70731f91bf22b7d45ce0e1bdb66fe3f9c2
Louis Claremont <lpclaremont@gmail.com>
2023-11-02 17:09:04 +0000

    links container added and CSS updated

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -8,12 +8,15 @@ function Home() {
         <div className="Home">
             <h1>Start your journey towards healthier, happier living</h1>
             <div className="summaryContainer">
-                <div className="textSummary">
-                    {/* Add your text summary here */}
-                    <p>Your text summary goes here.</p>
+                <div className="infoContainer">
+                <div className="textContainer">
+                    <p>Lift & nourish is here to help you! Life long habits aren't easy to start but once maintained, you'll barely notice them.</p>
                 </div>
-                <div className="image-container">
-                    {/* Add your image here */}
+                <div className="linksContainer">
+                    <p>Useful links go here!</p>
+                </div>
+                </div>
+                <div className="imageContainer">
                     <img src={lauraHomepage} alt="Laura" />
                 </div>
             </div>
